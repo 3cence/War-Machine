b9b8bb6ea82d39fc74aa8a8b55a217814556157f
Emma <emmaalderson24@gmail.com>
2022-08-05 18:04:35 -0400

    Set up environment

---
+++ build/CMakeFiles/war.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/war.dir/main.cpp.o"
+  "war"
+  "war.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/war.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
